ee37ed0db61b1d56be4ce47c0ba80ef010dacfb3
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-09-16 12:02:33 +0530

    import java.util.Scanner; class Input {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         System.out.print("");         String str=sc.nextLine();         System.out.print(""+str);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Input
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("");
+        String str=sc.nextLine();
+        System.out.print(""+str);
+    }
+}
