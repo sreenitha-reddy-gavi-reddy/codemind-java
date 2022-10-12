34c15bc5a7f2e6cec98db812199622a8b247ae22
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-10-12 12:19:05 +0530

    import java.util.Scanner; class bigggg {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,sum=0,c=0;         float avg;         int []arr=new int[100];         n=sc.nextInt();         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();             sum+=arr[i];         }         avg=sum/n;         for(i=0;i<n;i++)         {             if(arr[i]>=avg)             {                 c+=1;             }         }         System.out.print(c);     } }

---
+++ elements_greater_than_average.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class bigggg
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,sum=0,c=0;
+        float avg;
+        int []arr=new int[100];
+        n=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            sum+=arr[i];
+        }
+        avg=sum/n;
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]>=avg)
+            {
+                c+=1;
+            }
+        }
+        System.out.print(c);
+    }
+}
