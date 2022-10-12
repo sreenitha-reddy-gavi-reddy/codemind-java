ecbd788131e8268222303531ff9966c0ccf2f1a5
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-10-12 11:42:57 +0530

    import java.util.Scanner; class countt {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int i,n,c,j,d=0,sum=0;         n=sc.nextInt();         int a[]=new int[n];         int b[]=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int k=sc.nextInt();         for(i=0;i<n;i++)         {             c=1;             if(a[i]>0)             {                 for(j=i+1;j<n;j++)                 {                     if(a[i]==a[j])                     {                         c+=1;                         a[j]=-1;                     }                 }                 if(c==k)                 {                     d=1;                     System.out.print(a[i]+" ");                 }             }         }         if(d==0)         {             System.out.println("-1");         }     } }

---
+++ N_times_repeated_elements.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class countt
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,n,c,j,d=0,sum=0;
+        n=sc.nextInt();
+        int a[]=new int[n];
+        int b[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            c=1;
+            if(a[i]>0)
+            {
+                for(j=i+1;j<n;j++)
+                {
+                    if(a[i]==a[j])
+                    {
+                        c+=1;
+                        a[j]=-1;
+                    }
+                }
+                if(c==k)
+                {
+                    d=1;
+                    System.out.print(a[i]+" ");
+                }
+            }
+        }
+        if(d==0)
+        {
+            System.out.println("-1");
+        }
+    }
+}
