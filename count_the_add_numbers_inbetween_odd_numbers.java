e6f0b21b3ebeeaa50cadf1719233a2798f64b814
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-10-12 12:30:37 +0530

    import java.util.Scanner; class countff {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,c=0;         n=sc.nextInt();         int a[]=new int[100];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=1;i<n;i++)         {             if(a[i]%2!=0)             {                 if(a[i-1]%2!=0 && a[i+1]%2!=0)                 {                     if(i!=n-1)                     {                         c+=1;                     }                 }             }         }         System.out.println(c);     } }

---
+++ count_the_add_numbers_inbetween_odd_numbers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class countff
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,c=0;
+        n=sc.nextInt();
+        int a[]=new int[100];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=1;i<n;i++)
+        {
+            if(a[i]%2!=0)
+            {
+                if(a[i-1]%2!=0 && a[i+1]%2!=0)
+                {
+                    if(i!=n-1)
+                    {
+                        c+=1;
+                    }
+                }
+            }
+        }
+        System.out.println(c);
+    }
+}
