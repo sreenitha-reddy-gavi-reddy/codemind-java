b0b94ba9c7259aba2a6624398e7afaf0f3e250fa
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-10-12 13:15:03 +0530

    import java.util.Scanner; class minimum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i;         int []arr=new int[100];         n=sc.nextInt();         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(i=n-1;i>=0;i--)         {             if(arr[i]%2==0)             {                 System.out.print(arr[i]);                 break;             }         }     } }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class minimum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i;
+        int []arr=new int[100];
+        n=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=n-1;i>=0;i--)
+        {
+            if(arr[i]%2==0)
+            {
+                System.out.print(arr[i]);
+                break;
+            }
+        }
+    }
+}
